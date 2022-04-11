1efcc4cd7ff1dd2b7b79cb65f749bec77f2c2eeb
ritesh <riteshsingh.py@gmail.com>
2022-04-11 11:25:16 +0530

    Added basic navbar :=)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import ShortContact from "./components/shortContact";
 import Slider from "./components/slider";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
       <ShortContact />
+      <Navbar />
       <Slider />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,39 @@
 import React from "react";
+import "./Navbar.css";
+import { ReactComponent as Logo } from "../assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="logo">
-        <h3>Ritesh</h3>
-      </div>
-      <div className="home">
-        <ul>
-          <li>Home</li>
-          <li>Contact</li>
-          <li>About Us</li>
-          <li>Portfolio</li>
-        </ul>
+    <div>
+      <header>
+        <a class="logo" href="/">
+          <Logo />
+        </a>
+        <nav>
+          <ul class="nav__links">
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </nav>
+        <a class="cta" href="#">
+          Contact
+        </a>
+        <p class="menu cta">Menu</p>
+      </header>
+      <div class="overlay">
+        <a class="close">&times;</a>
+        <div class="overlay__content">
+          <a href="#">Services</a>
+          <a href="#">Projects</a>
+          <a href="#">About</a>
+        </div>
       </div>
     </div>
   );
